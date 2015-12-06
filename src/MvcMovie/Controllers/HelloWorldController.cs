c8a601297a98d8596ea0216a6848f1447caa3cba
CourtneyOleary <courtney.j.oleary@gmail.com>
2015-12-07 12:19:00 +1300

    initial commit

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "this is the Welcome action method...";
+        }
+    }
+}
